d625d9edbe69fb5d2b1999f2941397301c6e4f11
OlavoBorborema <olavohenriqueborborema@gmail.com>
2025-02-19 17:51:48 +0000

    Exemplo190225

---
+++ index.js
@@ -0,0 +1,9 @@
+const { pseudoRandomBytes } = require('crypto');
+const express = require('express');
+    const  app = express ();
+
+    const port = 3000;
+
+    app.listen (port, () => {
+        console.log('Servidor online na porta ' + port)
+    });
